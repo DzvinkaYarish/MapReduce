49bd03aa285911e2219a28c7a1c56dfe37924428
dzvinka <yarish@ucu.edu.ua>
2018-04-17 20:14:15 +0300

    added example

--- map_reduce.py
+++ map_reduce.py
@@ -1,80 +1,108 @@
 import multiprocessing
 import fileinput
 import os
-DIRS = {'input':'input', 'mapper':'temp_mapper', 'reducer':'temp_reducer', 'output':'output'}
+DIRS = {'input': 'input/', 'mapper': 'temp_mapper/', 'reducer': 'temp_reducer/', 'output': 'output/'}
+
+# temp_mapper/reducer_1/mapper_1.txt, mapper_2.txt
+
+with open('configurations.txt') as file:
+    NUM_MAPPERS = int(file.readline().split('=')[1].strip())
+    NUM_REDUCERS = int(file.readline().split('=')[1].strip())
+    DATA_DIR = file.readline().split('=')[1].strip()
+    COMBINE = bool(file.readline().split('=')[1].strip())
+
 
 class Mapper(multiprocessing.Process):
     def __init__(self, index):
         super(Mapper, self).__init__()
         self.index = index
-        # multiprocessing.Process.init()
+
+    def __find_reducer(self, key):
+        return hash(key) % NUM_REDUCERS
 
     def map(self, element):
-        pass
+        print('sdfgh')
 
     def combine(self, mapper_results):
         pass
 
     def shuffle(self, mapper_results):
-
-
+        for rdr_i in range(NUM_REDUCERS):
+            if not os.path.exists('{}reducer_{}/'.format(DIRS['mapper'], rdr_i)):
+                print('ghj')
+                os.makedirs('{}reducer_{}/'.format(DIRS['mapper'], rdr_i))
+            file_for_ith_reducer = open('{}reducer_{}/mapper_{}.txt'.format(DIRS['mapper'], rdr_i, self.index), 'w+')
+            [file_for_ith_reducer.write(key + '\t' + value + '\n') for (key, value) in mapper_results] #if rdr_i == self.__find_reducer(key)]
+            file_for_ith_reducer.close()
 
     def run(self):
         for filename in os.listdir(DIRS['input']):
-            if filename.split('.').endswith(str(self.index)):
-                with open(filename, 'r') as file:
-                    mapper_results = self.map(file)
-
-
-
-
+            if filename.split('.')[0].endswith(str(self.index)):
+                with open(DIRS['input'] + filename, 'r') as file:
+                    mapper_results = self.map(file.readlines())
+                    if self.combine:
+                        mapper_results = self.combine(mapper_results)
+                    self.shuffle(mapper_results)
+                break
 
 
 class Reducer(multiprocessing.Process):
+    def __init__(self, index):
+        super(Reducer, self).__init__()
+        self.index = index
+        self.map = {}
+
     def reduce(self, key, value):
         pass
-    def run(self):
-        pass
 
-class Job():
-    def __init__(self, input_files, num_mappers, num_reducers, dirs):
-        self.input_files = input_files
-        self.num_mappers = num_mappers
-        self.num_reducers = num_reducers
-        self.dirs  = dirs
     def run(self):
+        for filename in os.listdir('{}reducer_{}/'.format(DIRS['mapper'], self.index)):
+            with open('{}reducer_{}/'.format(DIRS['mapper'], self.index) + filename, 'r') as file:
+                for line in file.readlines():
+                    self.reduce(line.split('\t')[0], line.split('\t')[1])
+        with open('{}output_{}.txt'.format(DIRS['reducer'], self.index), 'w') as file:
+            for (key, value) in self.map:
+                file.write(key + '\t' + value + '\n')
+
+
+class Job:
+
+    def run(self):
+        for dir in DIRS:
+            if not os.path.exists(DIRS[dir]):
+                os.makedirs(DIRS[dir])
         fhandler = FileHandler()
-        fhandler.split_files(self.input_files, self.num_mappers, self.dirs)
+        fhandler.split_files()
         map_workers = []
         rdc_workers = []
 
-        for i in range(self.num_mappers):
-            m = Mapper(i)
+        for map_indx in range(NUM_MAPPERS):
+            m = Mapper(map_indx)
             map_workers.append(m)
             m.start()
         [t.join() for t in map_workers]
         # run the reduce step
-        for thread_id in range(self.num_reducers):
-            r = Reducer()
+        for rdc_indx in range(NUM_REDUCERS):
+            r = Reducer(rdc_indx)
             rdc_workers.append(r)
             r.start()
         [t.join() for t in rdc_workers]
-        fhandler.join_files(self.dirs)
+        fhandler.join_files()
 
 
 class FileHandler():
-    def split_files(self, filenames, num_splits, dirs):
-        files = [open(dirs['input'] + '/input_%d.txt' % i, 'w') for i in range(num_splits)]
-        for filename in filenames:
-            with open(filename, 'r') as infp:
+    def split_files(self):
+        files = [open(DIRS['input'] + 'input_%d.txt' % i, 'w+') for i in range(NUM_MAPPERS)]
+        for filename in os.listdir(DATA_DIR):
+            with open(DATA_DIR + filename, 'r') as infp:
                 for i, line in enumerate(infp):
-                    files[i % num_splits].write(line)
+                    files[i % NUM_MAPPERS].write(line)
         for f in files:
             f.close()
 
-    def join_files(self, dirs):
-        with open(dirs['output'] + '/result.txt', 'w') as out_file:
-            f_to_join = [f for f in os.listdir(dirs['reducer'])]
+    def join_files(self):
+        with open('{}result.txt'.format(DIRS['output']), 'w+') as out_file:
+            f_to_join = [DIRS['reducer'] + f for f in os.listdir(DIRS['reducer'])]
             allfiles = fileinput.input(f_to_join)
             for l in allfiles:
                 out_file.write(l)
---
+++ word_count_example.py
@@ -0,0 +1,42 @@
+import itertools
+import map_reduce
+
+
+class WordCountMapper(map_reduce.Mapper):
+    def __init__(self, index):
+        map_reduce.Mapper.__init__(self, index)
+
+    def map(self, element):
+        results = []
+        print(element)
+        for line in element:
+            values = line.split()
+            results.extend([(value, 1) for value in values if value.isalpha()])
+            print(values)
+        return results
+
+    def combine(self,  mapper_results):
+        return [(sum(i[0] for i in group), key) for key, group in
+            itertools.groupby(sorted(mapper_results, key=lambda i: i[0]), lambda i: i[1])]
+
+
+class WordCountReducer(map_reduce.Reducer):
+    def __init__(self, index):
+        map_reduce.Reducer.__init__(self, index)
+
+    def reduce(self, key, value):
+        if key in self.map:
+            self.map[key] += value
+        else:
+            self.map[key] = value
+
+
+if __name__ == "__main__":
+    word_count_job = map_reduce.Job()
+    word_count_job.run()
+
+
+
+
+
+
