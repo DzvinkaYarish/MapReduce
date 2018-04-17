696728b90c24ba7ee20e7cdb44c7cb6b0c7fb440
dzvinka <yarish@ucu.edu.ua>
2018-04-17 09:35:10 +0300

    update

--- map_reduce.py
+++ map_reduce.py
@@ -4,17 +4,31 @@ import os
 DIRS = {'input':'input', 'mapper':'temp_mapper', 'reducer':'temp_reducer', 'output':'output'}
 
 class Mapper(multiprocessing.Process):
-    # def __init__(self):
-    #     super.init()
-    #     pass
+    def __init__(self, index):
+        super(Mapper, self).__init__()
+        self.index = index
+        # multiprocessing.Process.init()
+
     def map(self, element):
         pass
-    def shuffle(self, key, value):
+
+    def combine(self, mapper_results):
         pass
-    def combine(self, key, value):
-        pass
+
+    def shuffle(self, mapper_results):
+
+
+
     def run(self):
-        pass
+        for filename in os.listdir(DIRS['input']):
+            if filename.split('.').endswith(str(self.index)):
+                with open(filename, 'r') as file:
+                    mapper_results = self.map(file)
+
+
+
+
+
 
 class Reducer(multiprocessing.Process):
     def reduce(self, key, value):
@@ -23,19 +37,19 @@ class Reducer(multiprocessing.Process):
         pass
 
 class Job():
-    def __init__(self, input_file, num_mappers, num_reducers, dirs):
-        self.input_file = input_file
+    def __init__(self, input_files, num_mappers, num_reducers, dirs):
+        self.input_files = input_files
         self.num_mappers = num_mappers
         self.num_reducers = num_reducers
         self.dirs  = dirs
     def run(self):
         fhandler = FileHandler()
-        fhandler.split_files(self.input_file, self.num_mappers, self.dirs)
+        fhandler.split_files(self.input_files, self.num_mappers, self.dirs)
         map_workers = []
         rdc_workers = []
 
         for i in range(self.num_mappers):
-            m = Mapper()
+            m = Mapper(i)
             map_workers.append(m)
             m.start()
         [t.join() for t in map_workers]
@@ -49,13 +63,14 @@ class Job():
 
 
 class FileHandler():
-    def split_files(self, filename, num_splits, dirs):
-        with open(filename, 'r') as infp:
-            files = [open(dirs['input'] + '/input_%d.txt' % i, 'w') for i in range(num_splits)]
-            for i, line in enumerate(infp):
-                files[i % num_splits].write(line)
-            for f in files:
-                f.close()
+    def split_files(self, filenames, num_splits, dirs):
+        files = [open(dirs['input'] + '/input_%d.txt' % i, 'w') for i in range(num_splits)]
+        for filename in filenames:
+            with open(filename, 'r') as infp:
+                for i, line in enumerate(infp):
+                    files[i % num_splits].write(line)
+        for f in files:
+            f.close()
 
     def join_files(self, dirs):
         with open(dirs['output'] + '/result.txt', 'w') as out_file:
